cbd39d1079b4619e886547c45461eb3a2ce24c88
Ahmed949916 <ahmed40152@gmail.com>
2025-03-21 14:38:45 +0500

    added svg config

--- my-shadcn-app/next.config.ts
+++ my-shadcn-app/next.config.ts
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config:any) {
+  webpack(config) {
+    // Add SVGR loader to handle SVGs as React components
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"], // Converts SVGs into React components
+      test: /\.svg$/, // Match all .svg files
+      use: ["@svgr/webpack"], // Use SVGR to transform SVGs into React components
     });
 
     return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"], // Use SVGR in Turbopack (experimental)
+          as: "*.js", // Treat SVGs as JS modules
+        },
+      },
+    },
   },
 };
 
